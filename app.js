5e76dd2422e880fca00e2659979cbf575d525da7
Matvey Pakhomov <matwick1@yandex.ru>
2022-06-01 11:30:51 +0400

    update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routes = require("./routes/index");
 const NotFoundError = require("./errors/not-found-err");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const { NODE_ENV, DB_ADRESS, PORT = 3000 } = process.env;
+const { NODE_ENV, DB_ADRESS, PORT = 3001 } = process.env;
 const app = express();
 mongoose.connect(
   NODE_ENV === "production"
