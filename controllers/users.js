633e8fbdc7bb700b9f3d43fee2dd4b024f2b219a
Matvey Pakhomov <matwick1@yandex.ru>
2022-07-03 19:10:17 +0400

    small fixes

--- controllers/users.js
+++ controllers/users.js
@@ -29,12 +29,12 @@ function login(req, res, next) {
         .status(200)
         .cookie("jwt", token, {
           maxAge: 3600000 * 24 * 7,
-          // secure: true,
-          // sameSite: false,
-          // domain:
-          //   NODE_ENV === "production"
-          //     ? "pakhomov.diploma.nomoredomains.work"
-          //     : false,
+          secure: true,
+          sameSite: "none",
+          domain:
+            NODE_ENV === "production"
+              ? "pakhomov.diploma.nomoredomains.work"
+              : false,
         })
         .send({ message: "Аутентификация пройдена" })
         .end();
